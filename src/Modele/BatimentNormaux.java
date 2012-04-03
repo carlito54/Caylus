d508a961908ec0c9b463fdbec144a127e53fbad5
Charles <godartcharles@gmail.com>
2012-04-03 22:19:57 +0200

    1

--- src/Modele/BatimentNormaux.java
+++ src/Modele/BatimentNormaux.java
@@ -1,11 +1,59 @@
 package Modele;
 
 public class BatimentNormaux extends Batiment{
-
-	public BatimentNormaux(String nom, Joueur proprio, boolean occupe, int position) {
-		super(nom, proprio, occupe, position);
+	
+	private Ressource[] cout;
+	private int prestige;
+	private int coutD;
+	private int faveur;
+	
+	public BatimentNormaux(String nom, Joueur prop, boolean occupe,int pos, int pre, Ressource[] cout,int coutD, int faveur) {
+		super(nom, prop, occupe, pos);
+		this.cout = cout;
+		this.prestige = prestige;
+		this.coutD = coutD;
+		this.faveur=faveur;
+	}
+	
+	public void appliquerEffet(Joueur j){
+		
 	}
 
+	public Ressource[] getCout() {
+		return cout;
+	}
+
+	public void setCout(Ressource[] cout) {
+		this.cout = cout;
+	}
+
+	public int getPrestige() {
+		return prestige;
+	}
+
+	public void setPrestige(int prestige) {
+		this.prestige = prestige;
+	}
+
+	public int getCoutD() {
+		return coutD;
+	}
+
+	public void setCoutD(int coutD) {
+		this.coutD = coutD;
+	}
+
+	public int getFaveur() {
+		return faveur;
+	}
+
+	public void setFaveur(int faveur) {
+		this.faveur = faveur;
+	}
+	
+	
+	
+	
 	
 	
 }
--- src/Modele/Chateau.java
+++ src/Modele/Chateau.java
@@ -1,5 +1,34 @@
 package Modele;
 
-public class Chateau {
+public class Chateau extends Batiment{
+
+	private Joueur[] ordreConst;
+	private int decompte;
+	
+	public Chateau(String nom, Joueur proprio, boolean occupe, int position, int decom, Joueur[] ordre) {
+		super(nom, proprio, occupe, position);
+		this.decompte = decom;
+		this.ordreConst = ordre;
+	}
+
+	public Joueur[] getOrdreConst() {
+		return ordreConst;
+	}
+
+	public void setOrdreConst(Joueur[] ordreConst) {
+		this.ordreConst = ordreConst;
+	}
+
+	public int getDecompte() {
+		return decompte;
+	}
+
+	public void setDecompte(int decompte) {
+		this.decompte = decompte;
+	}
+	
+	
+	
+	
 
 }
