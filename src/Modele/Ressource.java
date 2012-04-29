c3bbee8adb72a4e32148ed2e466ffce6a4c86e82
Loic Cimon <loic.cimon@laposte.net>
2012-04-29 14:29:54 +0200

    Modification Ressource : plus generique

--- src/Modele/Ressource.java
+++ src/Modele/Ressource.java
@@ -1,63 +1,50 @@
 package Modele;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ressource {
 
-	private int bois;
-	private int nourriture;
-	private int pierre;
-	private int tissu;
-	private int or;
-	
-	public Ressource(int bois, int nourriture, int pierre, int tissu, int or) {
-		super();
-		this.bois = bois;
-		this.nourriture = nourriture;
-		this.pierre = pierre;
-		this.tissu = tissu;
-		this.or = or;
-	}
-
-	public int getBois() {
-		return bois;
-	}
-
-	public void setBois(int bois) {
-		this.bois = bois;
-	}
-
-	public int getNourriture() {
-		return nourriture;
-	}
-
-	public void setNourriture(int nourriture) {
-		this.nourriture = nourriture;
-	}
-
-	public int getPierre() {
-		return pierre;
-	}
-
-	public void setPierre(int pierre) {
-		this.pierre = pierre;
-	}
-
-	public int getTissu() {
-		return tissu;
-	}
-
-	public void setTissu(int tissu) {
-		this.tissu = tissu;
-	}
-
-	public int getOr() {
-		return or;
-	}
-
-	public void setOr(int or) {
-		this.or = or;
-	}
-	
-	
-	
-	
+    private Map<String, Integer> ressources;
+    private final String[] nomRessources = {"Bois", "Nourriture", "Pierre", "Tissu", "Or"};
+    
+    /**
+     * Crée les ressources avec une quantité à 0
+     */
+    public Ressource() {
+        ressources = new HashMap<>();
+        for (String nom : nomRessources) {
+            ressources.put(nom, 0);
+        }
+    }
+    
+    /**
+     * Crée les ressources avec une quantité à 0
+     */
+    public Ressource(int[] qte) {
+        ressources = new HashMap<>();
+        for (int i = 0; i < qte.length; i++) {
+            ressources.put(nomRessources[i], qte[i]);
+        }
+    }
+    
+    /**
+     * Renvoie la quatité de la ressource dont le nom est en paramètre
+     * 
+     * @param nom nom de la ressource
+     * @return quantité de la ressource nom
+     */
+    public int getRessource(String nom) {
+        return ressources.get(nom);
+    }
+    
+    /**
+     * Modifie la quatité de la ressource nom
+     * 
+     * @param nom, nom de la ressource à modifier
+     * @param qte, nouvelle quantité
+     */
+    public void setRessource(String nom, int qte) {
+        ressources.put(nom, qte);
+    }
 }
